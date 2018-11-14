95a958094e9fc9cd9a06a00a932c8bf4ef4f98b4
onsan <onursanir@gmail.com>
2018-11-14 23:40:49 +0300

    Dosya güncellendi

--- Game.cpp
+++ Game.cpp
@@ -1,2 +1,44 @@
-onur
+#include "Game.h"
+
+Game::Game() : window(sf::VideoMode(WIDTH, HEIGHT), "Name of Application")
+{
+    window.setFramerateLimit(60);
+}
+
+void Game::run()
+{
+    while (window.isOpen())
+    {
+        processEvents();
+        update();
+        render();
+    }
+}
+
+void Game::processEvents()
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        }
+    }
+}
+
+void Game::update()
+{
+}
+
+void Game::render()
+{
+    window.clear();
+
+    //draw()
+
+    window.display();
+}
 
