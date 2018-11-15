9a7e20307347df548cb167c640b40fa1722ec2ff
osanir <onursanir@gmail.com>
2018-11-15 03:40:39 +0300

    Change Files

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 g++ -c *.cpp
-g++ *.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system -lGL -lsfml-audio
-./sfml-app 
+g++ *.o -o game -lsfml-graphics -lsfml-window -lsfml-system -lGL -lsfml-audio
+./game
 
